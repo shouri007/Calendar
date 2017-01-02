7f23363317e8ddab0104c7b7d944e3640d9c4b6f
Shouri Piratla <shouri.piratla9@gmail.com>
2017-01-02 15:24:17 +0530

    Add urls for CRUD operations

--- ccalendar/urls.py
+++ ccalendar/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$',views.index,name='index')
+	url(r'^$',views.index,name='index'),
+	url(r'new',views.createEvent,name='newEvent'),
+	url(r'edit',views.update,name='updateEvent'),
+	url(r'eventDetails',views.retreive,name='readEvent'),
+	url(r'delete',views.delete,name='deleteEvent')
 ]
