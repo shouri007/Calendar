5d058c40f683245e416051a2d3b01030c1576776
Aakash Muttineni <aakash.muttineni@gmail.com>
2017-01-04 16:21:35 +0530

    Google Calendar API Authentication (#3)

--- ccalendar/urls.py
+++ ccalendar/urls.py
@@ -2,9 +2,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$',views.index,name='index'),
-	url(r'new',views.createEvent,name='newEvent'),
-	url(r'edit',views.update,name='updateEvent'),
-	url(r'eventDetails',views.retreive,name='readEvent'),
-	url(r'delete',views.delete,name='deleteEvent')
+    url(r'^$', views.index, name='index'),
+    url(r'new', views.createEvent, name='newEvent'),
+    url(r'edit', views.update, name='updateEvent'),
+    url(r'eventDetails', views.retrieve, name='readEvent'),
+    url(r'delete', views.delete, name='deleteEvent'),
+    url(r'sync', views.googleSync, name='sync'),
+    url(r'events/(?P<month>\d{2})/(?P<year>\d{4})', views.events, name='events')
 ]
