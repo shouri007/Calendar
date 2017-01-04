97d326216df6e7d186cc9bb8d6a0d1c8107a0d68
mkash32 <aakash.muttineni@gmail.com>
2017-01-05 01:24:17 +0530

    Google Calendar API Integration * Retreive events from google calendar api * Filter for events within a particular month/year * Saving user credentials after authentication

--- ccalendar/urls.py
+++ ccalendar/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     url(r'eventDetails', views.retrieve, name='readEvent'),
     url(r'delete', views.delete, name='deleteEvent'),
     url(r'sync', views.googleSync, name='sync'),
+    url(r'authcallback', views.auth_return, name='authSuccess'),
     url(r'events/(?P<month>\d{2})/(?P<year>\d{4})', views.events, name='events')
 ]
